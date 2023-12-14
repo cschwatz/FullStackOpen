5a15f3d3de3b1fdb9bb0bd44bf3723d441b3c7ad
cschwatz <criistianrs@hotmail.com>
2023-12-14 18:22:20 -0300

    exercise 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -17,7 +17,7 @@ const Button = ({ onClick, text }) => {
 const Display = ({ counter, text }) => {
   return (
     <p>
-     {text} {counter}
+     {text} {counter} {text === 'positive' ? '%' : ''}
     </p>
   )
 }
@@ -27,10 +27,42 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [history, setHistory] = useState([])
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1) 
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setHistory(history.concat(1))
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setHistory(history.concat(0)) 
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setHistory(history.concat(-1))
+  }
+  const historySum = () => {
+    let sum = 0
+    history.forEach(num => sum += 1)
+    return sum
+  }
+  const historyAvg = () => {
+    let avg = 0
+    let total = historySum()
+    history.forEach(num => avg += num)
+    return (total > 0 ? avg / total : 0)
+  }
+  const historyPositive = () => {
+    let totalPositives = 0
+    let total = 0
+    history.forEach(num => {
+      if (num > 0) {
+        totalPositives++
+      }
+      total++
+    })
+    return (total > 0 ? (totalPositives / total) * 100 : 0)
+  }
 
   return (
     <div>
@@ -42,6 +74,9 @@ const App = () => {
       <Display counter={good} text={'good'} />
       <Display counter={neutral} text={'neutral'} />
       <Display counter={bad} text={'bad'} />
+      <Display counter={historySum()} text={'all'} />
+      <Display counter={historyAvg()} text={'average'} />
+      <Display counter={historyPositive()} text={'positive'} />
     </div>
   )
 }
