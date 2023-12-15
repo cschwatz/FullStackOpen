e779e6e6e92fc07c81b32b76d880311f2956c74c
cschwatz <criistianrs@hotmail.com>
2023-12-15 14:52:50 -0300

    exercise 1.12

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,12 +1,6 @@
 import { useState } from 'react'
 
-const Banner = ({ message }) => {
-  return (
-    <h1>{message}</h1>
-  )
-} 
-
-const Button = ({ onClick, text }) => {
+const NextButton = ({ text, onClick }) => {
   return (
     <button onClick={onClick}>
       {text}
@@ -14,115 +8,32 @@ const Button = ({ onClick, text }) => {
   )
 }
 
-const Display = ({ counter, text }) => {
-  return (
-    <p>
-     {text} {counter} {text === 'positive' ? '%' : ''}
-    </p>
-  )
-}
-
-const StatsTable = ({ goodValue, neutralValue, badValue, history }) => {
-  if (history.length === 0) {
-    return <p>No feedback given</p>
-  } else {
-    let total = 0
-    let avg = 0
-    let positives = 0
-    history.forEach(num => {
-      if (num > 0) {
-        positives++
-      }
-      total++
-      avg += num
-    })
-    let avgTotal = (total > 0 ? (avg / total) : 0)
-    let totalPositives = (total > 0 ? (positives / total) * 100 : 0)
-    return (
-      <Table 
-        good={goodValue} 
-        neutral={neutralValue} 
-        bad={badValue} 
-        all={total} 
-        average={avgTotal} 
-        positive={totalPositives} 
-      />
-    )
-  }
-}
-
-const Table = ({ good, neutral, bad, all, average, positive}) => {
-  return (
-    <table>
-        <tbody>
-          <tr>
-            <td>
-              <Display counter={good} text={'good'} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Display counter={neutral} text={'neutral'} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Display counter={bad} text={'bad'} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Display counter={all} text={'all'} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Display counter={average} text={'average'} />
-            </td>
-          </tr> 
-          <tr>
-            <td>
-              <Display counter={positive} text={'positive'} />
-            </td>
-          </tr> 
-        </tbody>
-      </table>
-  )
-}
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [history, setHistory] = useState([])
-
-  const handleGoodClick = () => {
-    setGood(good + 1)
-    setHistory(history.concat(1))
-  }
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-    setHistory(history.concat(0)) 
-  }
-  const handleBadClick = () => {
-    setBad(bad + 1)
-    setHistory(history.concat(-1))
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const handleNext = () => {
+    let nextValue = Math.floor(Math.random() * anecdotes.length)
+    console.log(selected)
+    console.log(nextValue)
+    setSelected(nextValue)
   }
 
   return (
     <div>
-      <Banner message={'give feedback'} />
-      <Button onClick={handleGoodClick} text={'good'} />
-      <Button onClick={handleNeutralClick} text={'neutral'} />
-      <Button onClick={handleBadClick} text={'bad'} />
-      <Banner message={'statistics'} />
-      <StatsTable 
-        goodValue={good}
-        neutralValue={neutral}
-        badValue={bad}
-        history={history}
-      />
+      <p>
+      {anecdotes[selected]}
+      </p>
+      <NextButton text={'next anecdote'} onClick={handleNext} />
     </div>
   )
 }
