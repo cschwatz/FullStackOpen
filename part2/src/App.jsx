d767c972a3194b6dee1b918967ab7cfa3d6e28bb
cschwatz <criistianrs@hotmail.com>
2024-01-04 16:24:31 -0300

    exercise 2.5

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,21 +1,4 @@
-const Part = ({part}) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Course = ({course}) => {
-  let total = course.parts.reduce((prev, current) => prev + current.exercises, 0);
-  return(
-    <>
-      <h1>{course.name}</h1>
-      {course.parts.map((part) => {
-        return (<Part key={part.id} part={part} />)
-      })}
-      <p><b>total of {total} exercises</b></p>
-    </>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
