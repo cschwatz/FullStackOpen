f09fcb4a2f4102a248b458cb9aa8cc8c7506b7e8
cschwatz <criistianrs@hotmail.com>
2024-01-16 09:49:26 -0300

    exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+    let likes = 0
+    blogs.forEach(blog => likes += blog.likes)
+    return likes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
