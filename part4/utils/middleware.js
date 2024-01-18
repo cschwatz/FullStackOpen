5e628fad58b790328c2ac55a73706be7c0b8f0a5
cschwatz <criistianrs@hotmail.com>
2024-01-18 17:10:40 -0300

    exercise 4.19

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -3,6 +3,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'ValidationError') {
         return response.status(401).json({error: error.message})
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({ error: error.message })
     }
 
     next(error)
