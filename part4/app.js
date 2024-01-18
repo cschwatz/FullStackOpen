27f00dd9f1f5891bebeb540d7d1185883e2361c2
cschwatz <criistianrs@hotmail.com>
2024-01-18 16:50:42 -0300

    exercise 4.18

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const errorHandler = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(errorHandler)
 
 module.exports = app
