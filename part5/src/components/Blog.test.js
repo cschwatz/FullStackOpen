a41d88b42cc329ffc400bc212e0f6a3de913dd38
cschwatz <criistianrs@hotmail.com>
2024-02-03 13:54:25 -0300

    exercise 5.14

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('Blog tests', () => {
@@ -17,5 +18,20 @@ describe('Blog tests', () => {
     expect(titleElement).toBeInTheDocument()
     expect(authorElement).toBeInTheDocument()
     expect(hiddenPart).toHaveStyle({ display: 'none' })
+  })
+
+  test('Blog renders url and likes if show button is clicked', async () => {
+    const blog = {
+      title: 'testing',
+      author: 'testingus',
+      url: 'testing.com/test'
+    }
+    const user = userEvent.setup()
+    render(<Blog blog={blog}/>)
+    const button = screen.getByText('show')
+    const hiddenPart = document.getElementsByClassName('hiddenBlogPart')[0]
+    expect(hiddenPart).toHaveStyle({ display: 'none' })
+    await user.click(button)
+    expect(hiddenPart).toHaveStyle({ display: 'block' })
   })
 })
