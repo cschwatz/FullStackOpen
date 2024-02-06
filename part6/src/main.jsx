aa20ef5586484edaa5ee0aa05a820d2eadb1ea34
cschwatz <criistianrs@hotmail.com>
2024-02-06 14:54:54 -0300

    exercise 6.3

--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -1,54 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { createStore } from 'redux'
-import reducer from './reducer'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
 
 const store = createStore(reducer)
 
-const App = () => {
-  const goodButton = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
-
-  const okButton = () => {
-    store.dispatch({
-      type: 'OK'
-    })
-  }
-
-  const badButton = () => {
-    store.dispatch({
-      type: 'BAD'
-    })
-  }
-
-  const resetButton = () => {
-    store.dispatch({
-      type: 'ZERO'
-    })
-  }
-
-  return (
-    <div>
-      <button onClick={goodButton}>good</button> 
-      <button onClick={okButton}>ok</button> 
-      <button onClick={badButton}>bad</button>
-      <button onClick={resetButton}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
