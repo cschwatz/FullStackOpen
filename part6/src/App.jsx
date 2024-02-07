ed74c12da394ba47aade60c699574bd72e61e8de
cschwatz <criistianrs@hotmail.com>
2024-02-07 17:36:00 -0300

    exercise 6.16

--- part6/src/App.jsx
+++ part6/src/App.jsx
@@ -5,16 +5,14 @@ import Notification from './components/Notification'
 import { useEffect } from 'react'
 import anecdoteService from './services/anecdotes'
 import { useDispatch } from 'react-redux'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   
 const dispatch = useDispatch()
 
 useEffect(() => {
-  anecdoteService
-    .getAll()
-    .then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  dispatch(initializeAnecdotes())
 }, [])
 
   return (
--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -4,10 +4,8 @@ import { Provider } from 'react-redux'
 import App from './App'
 import store from './components/store'
 
-const currentStore = store
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={currentStore}>
+  <Provider store={store}>
     <App />
   </Provider>
 )
