eaefea3344cca481292fe0032cd8f084adf6c1cb
cschwatz <criistianrs@hotmail.com>
2024-02-14 21:13:00 -0300

    exercise 7.8

--- part7/src/App.jsx
+++ part7/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const useField = (type) => {
@@ -15,70 +15,65 @@ const useField = (type) => {
   }
 }
 
-const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
   
   useEffect(() => {
-    if (name) {
-        axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
-        .then((response) => {
-          console.log(response.data)
-          const countryData = {
-            found: true,
-            data: response.data
-          }
-          setCountry(countryData)
-        })
-        .catch((error) => {
-          console.log('this country is not in the backend')
-          setCountry({found: false})
-        })
-    }
-  }, [name])
-  return country
-}
+    axios.get(baseUrl)
+      .then((response) => setResources(resources.concat(response.data)))
+  }, [])
 
-const Country = ({ country }) => {
-  if (!country) {
-    return null
+  const create = (resource) => {
+    axios.post(baseUrl, resource)
+      .then((response) => {
+        const newResource = response.data
+        setResources([...resources, newResource])
+      })
   }
 
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+  const service = {
+    create: create
   }
 
-  return (
-    <div>
-      <h3>{country.data.name.common} </h3>
-      <div>capital {country.data.capital[0]} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name.common}`}/>  
-    </div>
-  )
+  return [
+    resources, service
+  ]
 }
 
 const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
-  const country = useCountry(name)
+  const content = useField('text')
+  const name = useField('text')
+  const number = useField('text')
 
-  const fetch = (e) => {
-    e.preventDefault()
-    setName(nameInput.value)
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+  const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault()
+    noteService.create({ content: content.value })
+  }
+ 
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+    personService.create({ name: name.value, number: number.value})
   }
 
   return (
     <div>
-      <form onSubmit={fetch}>
-        <input {...nameInput} />
-        <button>find</button>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
       </form>
+      {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
-      <Country country={country} />
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br/>
+        number <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
     </div>
   )
 }
